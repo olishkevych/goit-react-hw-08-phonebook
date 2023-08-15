033093fa69d7bb9686960cac3027bcc94ee01942
olishkevych <olishkevych.natalia@gmail.com>
2023-08-15 19:58:28 +0200

    update navLink

--- src/components/StyledNavLink/StyledNavLink.jsx
+++ src/components/StyledNavLink/StyledNavLink.jsx
@@ -1,10 +1,12 @@
 import { NavLink } from 'react-router-dom';
 import { styled } from 'styled-components';
 
-export const StyledNavLink = styled(NavLink)`
+const StyledNavLink = styled(NavLink)`
   color: #e7eaf6;
 
   &.active {
     color: #8ecdeb;
   }
 `;
+
+export default StyledNavLink;
